dba31ccd98ce29a8c2f7cc12f4835db7328cb2b5
Richard Hughes <rhughes@aroraengineers.com>
2018-12-05 12:29:56 -0500

    removing dist

--- dist/app/main.js
+++
@@ -1,2 +0,0 @@
-//>>built
-define(["require","exports","./widgets/App"],function(c,a,b){Object.defineProperty(a,"__esModule",{value:!0});a.app=new b.default({container:document.getElementById("app")})});
--- dist/app/package.js
+++
@@ -1,2 +0,0 @@
-//>>built
-var profile=function(){return{resourceTags:{amd:function(a,b){return/\.js$/.test(a)}}}}();
